b939f06e94757a37fb7e3725ad5c5348d9d052cc
Md Anik Hossain <anikh499@gmail.com>
2022-07-29 18:59:22 +0600

    sucessfully review & add product api create

--- index.js
+++ index.js
@@ -6,123 +6,202 @@ const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const stripe = require("stripe")(process.env.PAYMENT_SECRET_KEY);
 const port = process.env.PORT || 5000
+
+
 //use middleware
 app.use(cors());
 app.use(express.json());
 
 
-
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mwk4o.mongodb.net/?retryWrites=true&w=majority`;
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-// async function run() {
-//     try {
-//       await client.connect();
-//       const productCollection = client.db("robotic").collection("products");
-//       const orderCollection = client.db("robotic").collection("order");
-//       const userCollection = client.db("robotic").collection("user");
-//       const reviewCollection = client.db("robotic").collection("review");
-//       const paymentCollection = client.db("robotic").collection("payments");
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bnedm.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
-    try {
-      await client.connect();
-      const productCollection = client.db("Robotic").collection("product");
-      const orderCollection = client.db("Robotic").collection("order");
-      const userCollection = client.db("Robotic").collection("user");
-      const reviewCollection = client.db("Robotic").collection("review");
-      const paymentCollection = client.db("Robotic").collection("payments");
+  try {
+    await client.connect();
+    const productCollection = client.db("Robotic").collection("product");
+    const orderCollection = client.db("Robotic").collection("order");
+    const userCollection = client.db("Robotic").collection("user");
+    const reviewCollection = client.db("Robotic").collection("review");
+    const paymentCollection = client.db("Robotic").collection("payments");
 
 
-/******verify JWT********/
-function verifyJWT(req,res,next){
-  const authHeader =req.headers.authorization;
-  if(!authHeader){
-    return res.status(401).send({message:'authorization'})
-  }
-  const token =authHeader.split(' ')[1];
-  // verify a token symmetric
-jwt.verify(token,process.env.ACCESS_TOKEN_SECRET, function(err, decoded) {
-  if(err){
-    return res.status(403).send({message:'Forbiden access'})
-  }
-  req.decoded=decoded;
-  next();
-});
-}
+    /******verify JWT********/
+    function verifyJWT(req, res, next) {
+      const authHeader = req.headers.authorization;
+      if (!authHeader) {
+        return res.status(401).send({ message: 'authorization' })
+      }
+      const token = authHeader.split(' ')[1];
+      // verify a token symmetric
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+          return res.status(403).send({ message: 'Forbiden access' })
+        }
+        req.decoded = decoded;
+        next();
+      });
+    }
 
 
-// /******verifyAddmin ********/
-//  const verifyAdmin=async(req,res,next)=>{
-//    const requerster = req.decoded.email;
-//    const requersterAccount = await userCollection.findOne({email:requerster});
-//    if(requersterAccount.role==='admin'){
-//  next();
-//    }else{
-//      res.status(403).send({message:"you are nont admin"})
-//    }
-//  }
+    // /******verifyAddmin ********/
+    //  const verifyAdmin=async(req,res,next)=>{
+    //    const requerster = req.decoded.email;
+    //    const requersterAccount = await userCollection.findOne({email:requerster});
+    //    if(requersterAccount.role==='admin'){
+    //  next();
+    //    }else{
+    //      res.status(403).send({message:"you are nont admin"})
+    //    }
+    //  }
 
 
 
-// ----------get all product---------
-    app.get("/product",async(req,res)=>{
-        const query = {};
-        const cursor = productCollection.find(query);
-        const products = await cursor.toArray();
-        res.send(products);
+    // ----------get all product---------
+    app.get("/product", async (req, res) => {
+      const query = {};
+      const cursor = productCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products);
     })
- 
-// ----------get oneproduct--------- 
-    app.get("/productid/:Id",async(req,res)=>{
-        const Id = req.params.Id
-         const query = {_id:ObjectId(Id)};
-         const getOneProduct = await productCollection.findOne(query);
-         res.send(getOneProduct)
-      })
-  
-/******get userorder information sent backend********/
-app.post('/order',async(req,res)=>{
-  const order = req.body;
-  const result = await  orderCollection.insertOne(order );
-  res.send(result)
-})
-// ----------get all order---------
-app.get("/orders",async(req,res)=>{
-  const query = {};
-  const cursor = orderCollection.find(query);
-  const order = await cursor.toArray();
-  res.send(order);
-})
-/******show per user order by email********/
-app.get('/order',verifyJWT,async(req,res)=>{
-  const userEmail = req.query.userEmail;
-  const decodedEmail = req.decoded.email;
-  if(userEmail===decodedEmail){
-    const query ={userEmail:userEmail};
-    const order =await orderCollection.find(query).toArray();
-    return res.send(order)
-  }else{
-    return res.status(403).send({message:'forbiden'})
-  }
 
-})
+    // ----------get oneproduct--------- 
+    app.get("/productid/:Id", async (req, res) => {
+      const Id = req.params.Id
+      const query = { _id: ObjectId(Id) };
+      const getOneProduct = await productCollection.findOne(query);
+      res.send(getOneProduct)
+    })
 
-    }
-    finally {
+    /******get userorder information sent backend********/
+    app.post('/order', async (req, res) => {
+      const order = req.body;
+      const result = await orderCollection.insertOne(order);
+      res.send(result)
+    })
+    // ----------get all order---------
+    app.get("/orders", async (req, res) => {
+      const query = {};
+      const cursor = orderCollection.find(query);
+      const order = await cursor.toArray();
+      res.send(order);
+    })
+    /******show per user order by email********/
+    app.get('/order', verifyJWT, async (req, res) => {
+      const userEmail = req.query.userEmail;
+      const decodedEmail = req.decoded.email;
+      if (userEmail === decodedEmail) {
+        const query = { userEmail: userEmail };
+        const order = await orderCollection.find(query).toArray();
+        return res.send(order)
+      } else {
+        return res.status(403).send({ message: 'forbiden' })
+      }
+
+    })
+    /******delete order by email********/
+    app.delete("/removeOrder/:Id", verifyJWT, async (req, res) => {
+      const Id = req.params.Id
+      const query = { _id: ObjectId(Id) };
+      const result = await orderCollection.deleteOne(query);
+      res.send(result)
+    })
+    /******order details by id per user********/
+    app.get('/orderId/:id', verifyJWT, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const order = await orderCollection.findOne(query);
+      res.send(order)
+    })
+    /******update user********/
+
+    app.put('/user/:email', async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET);
+      res.send({ result, token: token });
+    })
+
+    //ADMIN ROLL
+    app.put('/user/admin/:email', verifyJWT, async (req, res) => {
+      const email = req.params.email;
+      const filter = { email: email };
+      const updateDoc = {
+        $set: { role: "admin" },
+      };
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result);
+
+    })
+    app.get('/admin/:email', verifyJWT, async (req, res) => {
+      const email = req.params.email;
+      const user = await userCollection.findOne({ email: email });
+      const isAdmin = user?.role === 'admin';
+      res.send({ admin: isAdmin })
+    })
+    /******get all user********/
+    app.get('/user', verifyJWT, async (req, res) => {
+      const user = await userCollection.find().toArray();
+      res.send(user)
+    })
+    /******delete user by email********/
+    app.delete('/removeuser/:email', verifyJWT, async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email }
+      const result = await userCollection.deleteOne(query);
+      return res.send(result);
+    })
+
+    /******add product********/
+    app.post('/addProduct', verifyJWT, async (req, res) => {
+      const doctor = req.body;
+      const result = await productCollection.insertOne(doctor);
+      return res.send(result);
+    })
+
+    app.get('/addProduct', verifyJWT, async (req, res) => {
+      const doctor = await productCollection.find().toArray();
+      res.send(doctor)
+    })
+
+    app.delete("/addProduct/:Id", verifyJWT, async (req, res) => {
+      const Id = req.params.Id
+      const query = { _id: ObjectId(Id) };
+      const result = await productCollection.deleteOne(query);
+      res.send(result)
+    })
+    /******get user review********/
+    app.post('/review', async (req, res) => {
+      const review = req.body;
+      const result = await reviewCollection.insertOne(review);
+      res.send(result)
+    })
+    app.get('/review', async (req, res) => {
+      const review = await reviewCollection.find().toArray();
+      res.send(review)
+    })
+
     
-    }
+
   }
-  run().catch(console.dir);
+  finally {
+
+  }
+}
+run().catch(console.dir);
 
 
 
 
 app.get('/', (req, res) => {
-    res.send('Hello From Roboctic World https://vast-tor-95198.herokuapp.com/!!')
-  })
-   
-  app.listen(port, () => {
-    console.log(`Robotic app listening on port ${port}`)
-  })
+  res.send('Hello From Roboctic World https://vast-tor-95198.herokuapp.com/!!')
+})
+
+app.listen(port, () => {
+  console.log(`Robotic app listening on port ${port}`)
+})
